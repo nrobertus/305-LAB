61c7f9a0d38301cd058fbc0f1d6f741ab6b34056
Nathan Robertus <nathan.robertus@gmail.com>
2015-03-04 15:22:27 -0700

    Fixed some issues with the inheritance

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -3,101 +3,93 @@ class Element:
     _name='';
     def __init__(self, name):
         self._name = name
-    def name():
-        return _name
+    def name(self):
+        return self._name
     def compareTo(Element):
         raise NotImplementedError("Not yet implemented.")
 
 class Rock(Element):
-    def __init__(self, name="Rock"):
-        self._name = name
     def compareTo(self, Element):
         outputString = ""
         outcome = ""
-        if(Element._name == self._name):
-            outputString = self._name + " equals " + self._name
+        if(Element.name() == self.name()):
+            outputString = self.name() + " equals " + self.name()
             outcome = "Tie"
-        elif(Element._name == "Scissors"):
-            outputString = self._name + " crushes " + Element._name
+        elif(Element.name() == "Scissors"):
+            outputString = self.name() + " crushes " + Element.name()
             outcome = "Win"
-        elif(Element._name == "Lizard"):
-            outputString = self._name + " crushes " + Element._name
+        elif(Element.name() == "Lizard"):
+            outputString = self.name() + " crushes " + Element.name()
             outcome = "Win"
-        elif(Element._name == "Paper"):
-            outputString = Element._name + " covers " + self._name
+        elif(Element.name() == "Paper"):
+            outputString = Element.name() + " covers " + self.name()
             outcome = "Lose"
-        elif(Element._name == "Spock"):
-            outputString = Element._name +" vaporizes " + self._name
+        elif(Element.name() == "Spock"):
+            outputString = Element.name() +" vaporizes " + self.name()
             outcome = "Lose"
         return (outputString, outcome)
 
 
 class Paper(Element):
-    def __init__(self, name="Paper"):
-        self._name = name
     def compareTo(self, Element):
         outputString = ""
         outcome = ""
-        if(Element._name == self._name):
-            outputString = self._name + " equals " + self._name
+        if(Element.name() == self.name()):
+            outputString = self.name() + " equals " + self.name()
             outcome = "Tie"
-        elif(Element._name == "Rock"):
-            outputString = self._name + " covers " + Element._name
+        elif(Element.name() == "Rock"):
+            outputString = self.name() + " covers " + Element.name()
             outcome = "Win"
-        elif(Element._name == "Spock"):
-            outputString = self._name + " disproves " + Element._name
+        elif(Element.name() == "Spock"):
+            outputString = self.name() + " disproves " + Element.name()
             outcome = "Win"
-        elif(Element._name == "Scissors"):
-            outputString = Element._name + " cuts " + self._name
+        elif(Element.name() == "Scissors"):
+            outputString = Element.name() + " cuts " + self.name()
             outcome = "Lose"
-        elif(Element._name == "Lizard"):
-            outputString = Element._name +" eats " + self._name
+        elif(Element.name() == "Lizard"):
+            outputString = Element.name() +" eats " + self.name()
             outcome = "Lose"
         return (outputString, outcome)
 
 class Scissors(Element):
-    def __init__(self, name="Scissors"):
-        self._name= name
     def compareTo(self, Element):
         outputString = ""
         outcome = ""
-        if(Element._name == self._name):
-            outputString = self._name + " equals " + self._name
+        if(Element.name() == self.name()):
+            outputString = self.name() + " equals " + self.name()
             outcome = "Tie"
-        elif(Element._name == "Paper"):
-            outputString = self._name + " cut " + Element._name
+        elif(Element.name() == "Paper"):
+            outputString = self.name() + " cut " + Element.name()
             outcome = "Win"
-        elif(Element._name == "Lizard"):
-            outputString = self._name + " decapitate " + Element._name
+        elif(Element.name() == "Lizard"):
+            outputString = self.name() + " decapitate " + Element.name()
             outcome = "Win"
-        elif(Element._name == "Rock"):
-            outputString = Element._name + " crushes " + self._name
+        elif(Element.name() == "Rock"):
+            outputString = Element.name() + " crushes " + self.name()
             outcome = "Lose"
-        elif(Element._name == "Spock"):
-            outputString = Element._name +" smashes " + self._name
+        elif(Element.name() == "Spock"):
+            outputString = Element.name() +" smashes " + self.name()
             outcome = "Lose"
         return (outputString, outcome)
 
 class Lizard(Element):
-    def __init__(self, name="Lizard"):
-        self._name=name
     def compareTo(self, Element):
         outputString = ""
         outcome = ""
-        if(Element._name == self._name):
-            outputString = self._name + " equals " + self._name
+        if(Element.name() == self.name()):
+            outputString = self.name() + " equals " + self.name()
             outcome = "Tie"
-        elif(Element._name == "Spock"):
-            outputString = self._name + " poisons " + Element._name
+        elif(Element.name() == "Spock"):
+            outputString = self.name() + " poisons " + Element.name()
             outcome = "Win"
-        elif(Element._name == "Paper"):
-            outputString = self._name + " eats " + Element._name
+        elif(Element.name() == "Paper"):
+            outputString = self.name() + " eats " + Element.name()
             outcome = "Win"
-        elif(Element._name == "Rock"):
-            outputString = Element._name + " crushes " + self._name
+        elif(Element.name() == "Rock"):
+            outputString = Element.name() + " crushes " + self.name()
             outcome = "Lose"
-        elif(Element._name == "Scissors"):
-            outputString = Element._name +" decapitate " + self._name
+        elif(Element.name() == "Scissors"):
+            outputString = Element.name() +" decapitate " + self.name()
             outcome = "Lose"
         return (outputString, outcome)
 
@@ -105,20 +97,20 @@ class Spock(Element):
     def compareTo(self, Element):
         outputString = ""
         outcome = ""
-        if(Element._name == self._name):
-            outputString = self._name + " equals " + self._name
+        if(Element.name() == self.name()):
+            outputString = self.name() + " equals " + self.name()
             outcome = "Tie"
-        elif(Element._name == "Scissors"):
-            outputString = self._name + " smashes " + Element._name
+        elif(Element.name() == "Scissors"):
+            outputString = self.name() + " smashes " + Element.name()
             outcome = "Win"
-        elif(Element._name == "Rock"):
-            outputString = self._name + " vaporizes " + Element._name
+        elif(Element.name() == "Rock"):
+            outputString = self.name() + " vaporizes " + Element.name()
             outcome = "Win"
-        elif(Element._name == "Paper"):
-            outputString = Element._name + " disproves " + self._name
+        elif(Element.name() == "Paper"):
+            outputString = Element.name() + " disproves " + self.name()
             outcome = "Lose"
-        elif(Element._name == "Lizard"):
-            outputString = Element._name +" poisons " + self._name
+        elif(Element.name() == "Lizard"):
+            outputString = Element.name() +" poisons " + self.name()
             outcome = "Lose"
         return (outputString, outcome)
 
