b562514d391f910e4146483e41cf6a007121019a
Nathan Robertus <nathan.robertus@gmail.com>
2015-03-06 13:02:38 -0700

    Updated PyCharm interpreter

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -1,5 +1,21 @@
 from random import randint
 import itertools
+from Cheetah.Template import Template
+templateDef = """
+<HTML>
+<HEAD><TITLE>$title</TITLE></HEAD>
+<BODY>
+$contents
+## this is a single-line Cheetah comment and won't appear in the output
+#* This is a multi-line comment and won't appear in the output
+   blah, blah, blah
+*#
+</BODY>
+</HTML>"""
+
+nameSpace = {'title': 'Hello World Example', 'contents': 'Hello World!'}
+t = Template(templateDef, searchList=[nameSpace])
+print t
 
 class Element:
     #A class for each element in the RPCLS Game
