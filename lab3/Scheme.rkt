71e0c765142311b12eb74894326bd4eefc7a912d
brandonstrong <ultrabstrong@gmail.com>
2015-04-13 16:39:08 -0600

    Here is up to question 8 and the start to set

--- lab3/Scheme.rkt
+++ lab3/Scheme.rkt
@@ -1,4 +1,5 @@
 #lang racket
+; type in (f '(3 1 4 1 5 9))
 (define (f lst)
   (display lst)
   (newline)
@@ -9,6 +10,7 @@
   ; (c) ;
   (cons (+ 1 (car lst)) (f (cdr lst)))))
 
+; type in (member? 4 '(3 1 4 1 5 9))
 (define (member? e lst)
   ; This is basically an else if statement ;
   (cond
@@ -19,4 +21,8 @@
     ; if e is not equal to the first element in the list, return the recursive call of
     ; everything after the first element of the lsit ;
     (else (member? e (cdr lst))))
+)
+; type in (set? '(3 1 4 1 5 9))
+(define (set? lst)
+ (if #t "Fill this in" #t)
 )
